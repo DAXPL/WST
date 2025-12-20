5868c2f2fb2c2c03dfed84fb17afcea9321cc4c1
Cosinus215 <Cosinus215@gmail.com>
2025-12-21 00:32:37 +0200

    Add Servo

---
+++ WST-backend/Assets/Scripts/Servo.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Servo : MonoBehaviour {
+    [SerializeField] private Transform blades;
+    [Range(-1f,1f), SerializeField] private float angle;
+    [SerializeField] private float bladesRotationSpeed = 100;
+    private Coroutine _rotateBladesCoroutine;
+
+    [ContextMenu("Start Rotate Blades")]
+    public void StartRotateBlades() {
+        if (_rotateBladesCoroutine != null)
+            return;
+        
+        _rotateBladesCoroutine = StartCoroutine(RotateBlades());
+    } 
+    
+    [ContextMenu("Stop Rotate Blades")]
+    public void StopRotateBlades() {
+        if (_rotateBladesCoroutine == null) 
+            return;
+        
+        StopCoroutine(_rotateBladesCoroutine);
+        _rotateBladesCoroutine = null;
+    }
+
+    private IEnumerator RotateBlades() {
+        float step = 0;
+        while (true) {
+            step += bladesRotationSpeed * Time.deltaTime;
+            Quaternion baseRotation = Quaternion.Euler(90f * angle, 0f, 0f);
+            Quaternion spinRotation = Quaternion.AngleAxis(step, Vector3.up);
+            
+            blades.localRotation = baseRotation * spinRotation;
+            yield return null;
+        }
+    }
+}
