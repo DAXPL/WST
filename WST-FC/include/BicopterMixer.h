36a99f85efff0a6dd0b7e270dba1cbdcb9c350e9
Miłosz Klim <klim.milosz@gmail.com>
2025-12-23 19:53:01 +0100

    PID and EMA filter

--- WST-FC/include/BicopterMixer.h
+++ WST-FC/include/BicopterMixer.h
@@ -19,63 +19,56 @@ private:
 
     double _pitchSetpoint;
     double _pitchInput;
-    double _output;
-    double _kp = 0.5;  // Moc reakcji (Spring strength)
-    double _ki = 0.0;  // Pamięć błędu
-    double _kd = 0.8;  // Tłumienie
+    double _pitchOutput;
+    double _kpPitch = 1.5;
+    double _kiPitch = 0.0;
+    double _kdPitch = 0.1;
+    double _pitchFilterState = 0.0;
+    const double _pitchFilterAlpha = 0.7;
     PID *_pitchPID;
 
 public:
     BicopterMixer()
     {
-        _servoLeft = new ServoMotor(16,0,180); 
-        _servoRight = new ServoMotor(17,0,180);
+        _servoLeft = new ServoMotor(16); 
+        _servoRight = new ServoMotor(17);
 
         _pitchSetpoint = 0;
         _pitchInput = 0;
-        _output = 0;
+        _pitchOutput = 0;
 
-        _pitchPID = new PID(&_pitchInput, &_output, &_pitchSetpoint, _kp, _ki, _kd, DIRECT);
+        _pitchPID = new PID(&_pitchInput, &_pitchOutput, &_pitchSetpoint, _kpPitch, _kiPitch, _kdPitch, DIRECT);
     }
 
     void Init() override
     {
-        //if(_motorLeft) _motorLeft->Init();
-        //if(_motorRight) _motorRight->Init();
-        if(_servoLeft) _servoLeft->Init();
+        if(_servoLeft)  _servoLeft->Init();
         if(_servoRight) _servoRight->Init();
         
         if(_pitchPID)
         {
-            _pitchPID->SetMode(AUTOMATIC); // Włączenie PID
-            _pitchPID->SetSampleTime(10); // Częstotliwość odświeżania (ms)
+            _pitchPID->SetMode(AUTOMATIC);
+            _pitchPID->SetSampleTime(20); // PID refresh rate for servos - same as servo - 50hz!
             _pitchPID->SetOutputLimits(-9000, 9000);
         }
-        
     }
 
     void Update(DroneControlData *input, SensorsData *sensors) override 
     {
         if(sensors == nullptr || input == nullptr) return;
-        _pitchInput = sensors->pitch;
-        //_pitchSetpoint = input->pitch;
-        _pitchSetpoint = 0;
+        //EMA / LPF filter
+        _pitchFilterState = (_pitchFilterState * (1.0 - _pitchFilterAlpha)) + ((double)(sensors->pitch) * _pitchFilterAlpha);
 
+        _pitchInput = _pitchFilterState;
+        _pitchSetpoint = input->pitch;
+        
         if (_pitchPID->Compute()) 
         {
-            Serial.printf("%.2f\n", _output / 100.0f);
-
-            if(_servoLeft)
-            {
-                _servoLeft->Set(-_output);
-            } 
-
-            if(_servoRight)
-            {
-                _servoRight->Set(_output);
-            }
+            int16_t controlSignal = _pitchOutput;
+            Serial.println(controlSignal); 
+            if(_servoLeft) _servoLeft->Set(controlSignal);
+            if(_servoRight) _servoRight->Set(-controlSignal);
         }
-        
     }
 
     void StopAll() override
