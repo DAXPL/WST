018ecf51a91f696b373bd7a450fa544cbaadb2e3
Cosinus215 <Cosinus215@gmail.com>
2025-12-25 23:27:19 +0200

    Add proximity sensor layer mask

--- WST-backend/Assets/Scripts/ProximitySensor.cs
+++ WST-backend/Assets/Scripts/ProximitySensor.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class ProximitySensor : MonoBehaviour, IDigitalSensor {
     [SerializeField] private float hitDistance = 10f;
+    [SerializeField] private LayerMask hitMask;
     
     public object ReadValue() {
-        if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit, hitDistance)) {
+        if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit, hitDistance, hitMask)) {
             return hit.distance;
         }
         
