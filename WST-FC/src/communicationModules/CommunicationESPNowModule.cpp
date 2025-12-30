64f3e0f85c170f8107d7efb9b61c2bd08e262316
Mateeey0studia <mnawrot3@edu.cdv.pl>
2025-12-30 22:46:54 +0100

    add configuration espnow and messages with errors on connect

--- WST-FC/src/communicationModules/CommunicationESPNowModule.cpp
+++ WST-FC/src/communicationModules/CommunicationESPNowModule.cpp
@@ -5,10 +5,29 @@ CommunicationESPNowModule::CommunicationESPNowModule(DroneControlData *dataPtr, 
     sharedData = dataPtr;
     droneStatus = status;
 }
+uint8_t broadcastAddress[] = {0xEC,0x64,0xC9,0xC4,0xA2,0x1A}; //tu będzie do uzupełnienia MAC odbiorcy
+
+void CommunicationESPNowModule::setup(){
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+
+    if(esp_now_init() != ESP_OK){
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    esp_now_peer_info_t peerInfo = {};
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    if(esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+    }
+}
 
 void CommunicationESPNowModule::Init(){
-
-    WiFi.mode(WIFI_STA);
 
     Serial.print("MAC address: ");
     Serial.println(WiFi.macAddress());
